0b266dad3e6bc8174e3a55d1c7cac30a376d9bcd
Alexey Gubov <alexey.gubov@gmail.com>
2019-10-23 21:05:34 +0300

    удалил домашку из мастер-ветки

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -11,22 +11,11 @@ const MessageField = (props) => {
             <h1>Chat:</h1>
 
             {props.messages.map(message => <MessageComponent text={ message } />)}
-
-            <button onClick={addTestMessage}>click me</button>
         </div>
     );
 };
 
-const addTestMessage = () => {
-    messages.push('Нормально');
-    console.log(messages);
-    ManualRender();
-}
-
-const ManualRender = () => {
-    ReactDOM.render(
-        <MessageField messages={ messages } />,
-        document.getElementById('root'),
-    );
-}
-ManualRender();
+ReactDOM.render(
+    <MessageField messages={ messages } />,
+    document.getElementById('root'),
+);
