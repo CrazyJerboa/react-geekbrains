6234c643d3db948f2ba4de78ba3b28c937eb1962
Alexey Gubov <alexey.gubov@gmail.com>
2019-11-03 01:19:42 +0300

    ДЗ 4

--- static_src/components/ChatList.jsx
+++ static_src/components/ChatList.jsx
@@ -5,27 +5,47 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
 import {Link} from 'react-router-dom';
+import {FloatingActionButton, TextField} from "material-ui";
+import AddIcon from "material-ui/svg-icons/content/add";
+import PropTypes from "prop-types";
 
 export default class ChatList extends React.Component {
+    static propTypes = {
+        onHandleCreateChat: PropTypes.func,
+        chats: PropTypes.array
+    }
+
+    handleCreateChat() {
+        this.props.onHandleCreateChat();
+    }
+
     render() {
+        const { chats } = this.props;
+        const listItems = Object.keys(chats).map((key) => {
+
+            return (
+                <Link to={'/chat/' + key + '/'} key={key}>
+                    <ListItem button>
+                        <ListItemText primary={chats[key]['name']} secondary="Jan 9, 2014"/>
+                    </ListItem>
+                </Link>
+            )
+        });
+
         return (
-            <List className="list__users">
-                <Link to="/chat/1/">
-                    <ListItem button>
-                            <ListItemText primary="John Doe" secondary="Jan 9, 2014" />
-                    </ListItem>
-                </Link>
-                <Link to="/chat/2/">
-                    <ListItem button>
-                            <ListItemText primary="John Doe" secondary="Jan 9, 2014" />
-                    </ListItem>
-                </Link>
-                <Link to="/chat/3/">
-                    <ListItem button>
-                            <ListItemText primary="John Doe" secondary="Jan 9, 2014" />
-                    </ListItem>
-                </Link>
-            </List>
+            <div className="list__outer">
+                <List className="list__users">
+                    {listItems}
+                </List>
+
+                <FloatingActionButton
+                    className="add_chat_btn"
+                    title="Add new chat"
+                    onClick={ () => this.handleCreateChat() }
+                >
+                    <AddIcon />
+                </FloatingActionButton>
+            </div>
         );
     }
 }
